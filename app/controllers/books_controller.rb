e7452da8562f834fc9970244ffe72d591503bb11
kotatsuko <107920100+kotatsuko@users.noreply.github.com>
2022-07-03 08:45:48 +0000

    [finish]userのindexページ,bookのeditページ,bookのupdate,destroy機能　作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,10 @@ class BooksController < ApplicationController
   end
 
   def create
-    book=Book.new(book_params)
-    book.user_id=current_user.id
-    book.save
-    redirect_to book_path(book.id)
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def index
@@ -21,6 +21,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    @book=Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
     def destroy
@@ -33,7 +40,7 @@ class BooksController < ApplicationController
   private
 
     def book_params
-    params.permit(:title,:body)
+      params[:book].permit(:title,:body)
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   get "homes/about"=>"homes#about",as:"about"
   devise_for :users
   resources:users,only:[:index,:show,:edit]
-  resources:books,only:[:create,:index,:show,:edit,:destroy]
+  resources:books,only:[:create,:index,:show,:edit,:update,:destroy]
+  post "users" => "books#create"
   post "users/:id" => "books#create"
   post "books/:id" => "books#create"
-  post "users" => "books#create"
-  
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
